a28059e08d6e9904cc3e3c2829ba8e6441e90b8c
cjdepaul <cdepaulis123456@gmail.com>
2025-05-18 12:34:11 -0500

    Added some basic sidebar contents

--- space-app/src/components/AppSidebar.tsx
+++ space-app/src/components/AppSidebar.tsx
@@ -1,20 +1,72 @@
 import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
-  SidebarHeader,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar"
+
+import { SatelliteIcon, SunIcon, RocketIcon, CalendarDaysIcon, HouseIcon, HeartIcon } from "lucide-react"
  
+const sidebarItems = [
+  {
+    name: "Home",
+    url: "/",
+    icon: <HouseIcon />,
+  },
+  {
+    name: "Celestial Calendar",
+    url: "/celestial-calendar",
+    icon: <CalendarDaysIcon />,
+  },
+  {
+    name: "Satellites",
+    url: "/satellites",
+    icon: <SatelliteIcon />,
+  },
+  {
+    name: "Space Launches",
+    url: "/space-launches",
+    icon: <RocketIcon />,
+  },
+  {
+    name: "Space Weather",
+    url: "/space-weather",
+    icon: <SunIcon />,
+  },
+  {
+    name: "Ellen",
+    url: "/ellen",
+    icon: <HeartIcon />,
+  },
+]
+
+
 export function AppSidebar() {
   return (
-    <Sidebar>
-      <SidebarHeader />
+<Sidebar>
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {sidebarItems.map((item) => (
+                <SidebarMenuItem key={item.name}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      {item.icon}
+                      <span>{item.name}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
-      <SidebarFooter />
     </Sidebar>
   )
 }
