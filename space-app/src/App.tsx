dc4987267d5a155f815d774a1156d94e9ece69bb
cjdepaul <cdepaulis123456@gmail.com>
2025-05-18 10:48:51 -0500

    Starting to build things

--- space-app/src/App.tsx
+++ space-app/src/App.tsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
 import './App.css'
-import { Button } from "@/components/ui/button"
-import axios from 'axios';
-import {useQuery} from '@tanstack/react-query'
+import Layout from "@/components/layout"
+import { ThemeProvider } from '@/components/ThemeProvider';
+import Home from '@/pages/home';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const fetchStatus = async () => {
-  const res = await axios.get('http://localhost:8000/api/status');
-  return res.data;
-  };
-
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['status'],
-    queryFn: fetchStatus
-  });
-
-  if (isLoading) return <p>Loading...</p>;
-  if (isError) return <p>Error loading planet data.</p>;
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Api Call</h1>
-      <p>{data.status}</p>
-      <Button onClick={() => setCount(count + 1)} className="bg-amber-600">count is {count}</Button>
-      <div className="p-8 bg-red-500 text-white">
-        If this is red, Tailwind works.
+    <ThemeProvider defaultTheme='dark'>
+      <div className="App">
+        <Layout>
+          <Home />
+        </Layout>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
