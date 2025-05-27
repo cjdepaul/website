298b9e50801ee3baeb3507b1c50c88afec09691a
cjdepaul <cdepaulis123456@gmail.com>
2025-05-26 19:16:47 -0500

    prepare the backend architecture

--- spaceL-app/app/Http/Controllers/SpaceWeatherController.php
+++ spaceL-app/app/Http/Controllers/SpaceWeatherController.php
@@ -10,7 +10,63 @@ class SpaceWeatherController extends Controller
     public function getSpaceWeather()
     {
         return response()->json([
-            'weather' => "Hello, Space Weather!"
+            'current_scales' => $this->getcurrentScales(),
+            'speed' => $this->getSpeed(),
+            'density' => $this->getDensity(),
+            'bz' => $this->getBz(),
+            'bt' => $this->getBt(),
+            'kp' => $this->getKp(),
+            'CME' => $this->getCme(),
+            'flares' => $this->getFlares(),
+            'Aurora' => $this->getAurora()
+
         ]);
     }
+
+    private function getcurrentScales()
+    {
+        // Logic to get current scales
+        return 'Current Scales Data';
+    }
+    private function getSpeed()
+    {
+        // Logic to get speed
+        return 'Speed Data';
+    }
+    private function getDensity()
+    {
+        // Logic to get density
+        return 'Density Data';
+    }
+    private function getBz()
+    {
+        // Logic to get Bz
+        return 'Bz Data';
+    }
+    private function getBt()
+    {
+        // Logic to get Bt
+        return 'Bt Data';
+    }
+    private function getKp()
+    {
+        // Logic to get Kp
+        return 'Kp Data';
+    }
+    private function getCme()
+    {
+        // Logic to get CME
+        return 'CME Data';
+    }
+    private function getFlares()
+    {
+        // Logic to get Flares
+        return 'Flares Data';
+    }
+    private function getAurora()
+    {
+        // Logic to get Aurora
+        return 'Aurora Data';
+    }
+
 }
